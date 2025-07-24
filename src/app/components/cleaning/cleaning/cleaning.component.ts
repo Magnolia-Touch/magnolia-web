ca6da94b0f0b1797c7fb121c4f4531cdebe60319
cuddleminddev <cuddleminddev@gmail.com>
2025-07-24 14:56:20 +0530

    cleaning

--- src/app/components/cleaning/cleaning/cleaning.component.ts
+++ src/app/components/cleaning/cleaning/cleaning.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../shared/header/header.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-cleaning',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './cleaning.component.html',
   styleUrl: './cleaning.component.css'
 })
