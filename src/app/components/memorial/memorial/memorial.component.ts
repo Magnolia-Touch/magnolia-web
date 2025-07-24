6504431d37f46b19ad4c1a697bbc63686266a44f
cuddleminddev <cuddleminddev@gmail.com>
2025-07-24 17:16:07 +0530

    headings

--- src/app/components/memorial/memorial/memorial.component.ts
+++ src/app/components/memorial/memorial/memorial.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../shared/header/header.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-memorial',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './memorial.component.html',
   styleUrl: './memorial.component.css'
 })
