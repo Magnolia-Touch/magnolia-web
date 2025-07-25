a9a54e2bcaf0fc456350f3661089d48c3113de5e
cuddleminddev <cuddleminddev@gmail.com>
2025-07-25 08:46:21 +0530

    headings

--- src/app/components/memorial/journey/journey.component.ts
+++ src/app/components/memorial/journey/journey.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../shared/header/header.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-journey',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './journey.component.html',
   styleUrl: './journey.component.css'
 })
