dcd8297367e64badb4ea76a8784cda65b086c6c1
cuddleminddev <cuddleminddev@gmail.com>
2025-08-01 11:38:37 +0530

    login button change

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/interceptor/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(
+    public authService: AuthService
+  ){}
 
 }
