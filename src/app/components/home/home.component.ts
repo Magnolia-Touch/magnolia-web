fe0f85eb8ea31f5e7eb275b3bf6d5d4ee4ecc84f
cuddleminddev <cuddleminddev@gmail.com>
2025-08-01 11:44:35 +0530

    test route

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
