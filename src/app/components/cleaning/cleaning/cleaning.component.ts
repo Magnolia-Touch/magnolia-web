76c083ca33e324f40a99148a888c085c497fd4f6
cuddleminddev <cuddleminddev@gmail.com>
2025-08-21 10:59:08 +0530

    UI changes basic

--- src/app/components/cleaning/cleaning/cleaning.component.ts
+++ src/app/components/cleaning/cleaning/cleaning.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../shared/header/header.component";
 import { FooterComponent } from "../../../shared/footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cleaning',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './cleaning.component.html',
   styleUrl: './cleaning.component.css'
 })
