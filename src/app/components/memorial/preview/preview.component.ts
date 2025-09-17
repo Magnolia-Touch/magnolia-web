c9caf9ed71f665952e723616f01d1adcecca8fa2
cuddleminddev <cuddleminddev@gmail.com>
2025-09-17 13:40:29 +0530

    preview

--- src/app/components/memorial/preview/preview.component.ts
+++ src/app/components/memorial/preview/preview.component.ts
@@ -9,36 +9,36 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './preview.component.css'
 })
 export class PreviewComponent implements OnInit {
-  @Input() memorialForm?: any; 
-
+  @Input() memorialForm?: any;
   data: any;
+  profilePhotoUrlValue: string | null = null;
 
   ngOnInit(): void {
-    this.data = this.memorialForm || {
-      firstName: 'Kuriakose',
-      lastName: 'George',
-      dob: { year: 1990, month: 5, day: 15 },
-      dop: { year: 2025, month: 9, day: 30 },
-      location: 'Ernakulam',
-      facebook: 'https://facebook.com/kuriakose',
-      twitter: 'https://twitter.com/kuriakose',
-      instagram: 'https://instagram.com/kuriakose',
-      journey: 'Kuriakose had a beautiful life, full of memories and achievements.',
-      familyMembers: [
-        { name: 'Das', relation: 'Brother' },
-        { name: 'Anu', relation: 'Sister' }
-      ],
-      lifeEvents: [
-        { year: '2009', event: 'Graduated from school' },
-        { year: '2015', event: 'Started first job' }
-      ],
-      gallery: [
-        '/step4.png',
-        '/services2.png',
-        '/imgp2.png'
-      ],
-      profilePhoto: '/services3.png'
-    };
+    if (this.memorialForm) {
+      this.data = {
+        firstName: this.memorialForm.get('firstName')?.value,
+        lastName: this.memorialForm.get('lastName')?.value,
+        dob: this.memorialForm.get('dob')?.value,
+        dop: this.memorialForm.get('dop')?.value,
+        location: this.memorialForm.get('location')?.value,
+        facebook: this.memorialForm.get('facebook')?.value,
+        twitter: this.memorialForm.get('twitter')?.value,
+        instagram: this.memorialForm.get('instagram')?.value,
+        journey: this.memorialForm.get('journey')?.value,
+        familyMembers: this.memorialForm.get('familyMembers')?.value || [],
+        lifeEvents: this.memorialForm.get('lifeEvents')?.value || [],
+        gallery: this.memorialForm.get('gallery')?.value || [],
+        profilePhoto: this.memorialForm.get('profilePhoto')?.value || null
+      };
+
+      // Generate profile photo URL once
+      const file = this.data.profilePhoto;
+      if (file && typeof file !== 'string') {
+        this.profilePhotoUrlValue = URL.createObjectURL(file);
+      } else {
+        this.profilePhotoUrlValue = file;
+      }
+    }
   }
 
   getFormattedDate(date: any): string {
@@ -48,8 +48,6 @@ export class PreviewComponent implements OnInit {
   }
 
   get profilePhotoUrl(): string | null {
-    const file = this.data.profilePhoto;
-    if (!file) return null;
-    return typeof file === 'string' ? file : null;
+    return this.profilePhotoUrlValue;
   }
 }
