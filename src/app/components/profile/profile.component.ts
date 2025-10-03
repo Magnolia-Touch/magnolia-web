7151db73daf944cfc2521054ee4c039191c8c849
kuriakose <kuriakose00@gmail.com>
2025-10-03 09:30:35 +0530

    pagination

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -338,4 +338,17 @@ export class ProfileComponent implements OnInit {
     this.router.navigate(['/page', profileId]);
   }
 
+  getDisplayedPages(current: number, last: number): number[] {
+    const delta = 2;
+    const range: number[] = [];
+    for (let i = Math.max(2, current - delta); i <= Math.min(last - 1, current + delta); i++) {
+      range.push(i);
+    }
+    if (current - delta > 2) range.unshift(-1);
+    if (current + delta < last - 1) range.push(-2);
+    range.unshift(1);
+    if (last > 1) range.push(last);
+    return range;
+  }
+
 }
